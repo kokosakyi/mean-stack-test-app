01a6a53da0b6a8ee8277e0b8c566f44e3df0f210
Nana Andoh <conradkyei@gmail.com>
2017-07-27 09:11:42 -0400

    Finished code

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+
+import { AuthService } from './services/auth.service';
+import { BlogService } from './services/blog.service';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { EditBlogComponent } from './components/blog/edit-blog/edit-blog.component';
+import { DeleteBlogComponent } from './components/blog/delete-blog/delete-blog.component';
+import { PublicProfileComponent } from './components/public-profile/public-profile.component';
+
 
 
 const appRoutes: Routes = [
@@ -16,10 +30,42 @@ const appRoutes: Routes = [
   },
   { path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canActivate: [NotAuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [NotAuthGuard]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+   {
+    path: 'user/:username',
+    component: PublicProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'edit-blog/:id',
+    component: EditBlogComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'delete-blog/:id',
+    component: DeleteBlogComponent,
+    canActivate: [AuthGuard]
   },
   { path: '**', component: HomeComponent }
 ];
@@ -30,16 +76,26 @@ const appRoutes: Routes = [
     NavbarComponent,
     HomeComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent,
+    BlogComponent,
+    EditBlogComponent,
+    DeleteBlogComponent,
+    PublicProfileComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    FlashMessagesModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [AuthService, BlogService, AuthGuard, NotAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const authentication = require('./routes/authentication')(router);
+const blogs = require('./routes/blog')(router);
 
-const port = 8080; 
+const port = process.env.port || 8080; 
 
 // Set up mongoose
 mongoose.Promise = global.Promise;
@@ -18,12 +20,16 @@ mongoose.connect(config.uri, (err)=>{
     console.log("Can't connect to db: ", err);
   }
   else {
-    console.log("Secret: ", config.secret)
+    //console.log("Secret: ", config.secret)
     console.log("Connected to database: ", config.db);
   }
 });
 
 // Middleware
+// cors
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -31,6 +37,8 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/dist/'));
 // routes
 app.use('/authentication', authentication);
+app.use('/blogs', blogs);
+
 
 app.get('*', (req, res) => {
   //res.send('It is well with my soul');
